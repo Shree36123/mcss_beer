2279ec1de54900e4fcb1c15c438a437eecb9dd69
Shree <reachoutshree@gmail.com>
2021-06-15 07:48:15 +0530

    intial push. project created Close#2

---
+++ src/main/java/com/shree/microservices/mcss_beer/McssBeerApplication.java
@@ -0,0 +1,13 @@
+package com.shree.microservices.mcss_beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McssBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McssBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shree/microservices/mcss_beer/McssBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shree.microservices.mcss_beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McssBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
